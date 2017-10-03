74e0f4564b6f6422394883b39e630d75e121bba8
Kamil Bednarz <kamil.bednarz@u2i.com>
2017-10-03 23:40:34 +0200

    Improve form validation

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,30 @@
 var onContctUsSubmit = function () {
-    $(document).ready(function () {
-        $(".thanks").hide();
-        $(".sending").hide();
-        $(".errors").hide();
-        if ($("#contactForm input[name='name']").val() === "" ||
+    var form = document.getElementById('contactForm');
+
+    $(".thanks").hide();
+    $(".errors").hide();
+
+    if (grecaptcha.getResponse() !== '') { 
+        if (form.checkValidity()) {
+            // form valid, let's hide the form
+            $(form).hide();
+            // let's submit the form in background
+            $.post(form.action, JSON.stringify({
+                name: form.name,
+                email: form.email,
+                message: form.message,
+                'g-recaptcha-response': grecaptcha.getResponse()
+            }));
+            $('.thanks > div').addClass('wow fadeInUp animated');
+            $('.thanks').show();
+        } else {
+            $('.g-recaptcha-response').addClass('error');
+        }
+    } else { 
+        // Show missing recaptcha message
+    }
+    /*
+    if ($("#contactForm input[name='name']").val() === "" ||
             $("#contactForm input[name='email']").val() === "" || 
             grecaptcha.getResponse() === '') {
             $(".errors").show();
@@ -24,8 +45,10 @@ var onContctUsSubmit = function () {
             }, 'json');
         }
     });
+*/
     return false;
 };
+
 $(document).ready(function () {
     $(".thanks").hide();
     $(".sending").hide();
